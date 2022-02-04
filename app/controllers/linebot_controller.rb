40d0bc9e1016666a451e67ee0c295da6cc8ca734
Ubuntu <ubuntu@ip-172-31-22-170.ap-northeast-1.compute.internal>
2022-02-04 01:31:17 +0000

    for second-deploy

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -6,6 +6,7 @@ class LinebotController < ApplicationController
 
     def client
       @client ||= Line::Bot::Client.new { |config|
+        config.channel_id = ENV["LINE_CHANNEL_ID"]
         config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
         config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
       }
